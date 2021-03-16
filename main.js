f2ca0125bb3b33a3eac3e21288e54fb142865298
Takanobu <61861946+takanobu68@users.noreply.github.com>
2021-03-17 05:10:08 +0900

    全部のsphereに違う動画を設定

--- main.js
+++ main.js
@@ -17,13 +17,27 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(container.clientWidth, container.clientHeight);
 container.appendChild(renderer.domElement);
 
-const movie = document.getElementById("three-video5");
-movie.src = "./assets/video05.mp4";
-const videoTexture = new THREE.VideoTexture(movie);
+const movie1 = document.getElementById("three-video1");
+const movie2 = document.getElementById("three-video2");
+const movie3 = document.getElementById("three-video3");
+const movie4 = document.getElementById("three-video4");
+const movie5 = document.getElementById("three-video5");
+
+movie1.src = "./assets/video06.mp4";
+movie2.src = "./assets/video02.mp4";
+movie3.src = "./assets/video03.mp4";
+movie4.src = "./assets/video07.mp4";
+movie5.src = "./assets/video05.mp4";
+
+const videoTexture1 = new THREE.VideoTexture(movie1);
+const videoTexture2 = new THREE.VideoTexture(movie2);
+const videoTexture3 = new THREE.VideoTexture(movie3);
+const videoTexture4 = new THREE.VideoTexture(movie4);
+const videoTexture5 = new THREE.VideoTexture(movie5);
 
 const geometry = new THREE.PlaneGeometry(30, 25);
 const material = new THREE.MeshBasicMaterial({
-  map: videoTexture,
+  map: videoTexture5,
   side: THREE.DoubleSide,
 });
 const plane = new THREE.Mesh(geometry, material);
@@ -39,30 +53,44 @@ scene.add(plane2);
 
 const sphereGeometry = new THREE.SphereGeometry(3, 32, 32);
 const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-const spheremovieMaterial = new THREE.MeshBasicMaterial({ map: videoTexture });
+const spheremovieMaterial1 = new THREE.MeshBasicMaterial({
+  map: videoTexture1,
+});
+const spheremovieMaterial2 = new THREE.MeshBasicMaterial({
+  map: videoTexture2,
+});
+const spheremovieMaterial3 = new THREE.MeshBasicMaterial({
+  map: videoTexture3,
+});
+const spheremovieMaterial4 = new THREE.MeshBasicMaterial({
+  map: videoTexture4,
+});
+const spheremovieMaterial5 = new THREE.MeshBasicMaterial({
+  map: videoTexture5,
+});
 
-const sphere = new THREE.Mesh(sphereGeometry, spheremovieMaterial);
+const sphere = new THREE.Mesh(sphereGeometry, spheremovieMaterial5);
 
 sphere.position.set(0, 0, 10);
 scene.add(sphere);
 
-const sphereA = new THREE.Mesh(sphereGeometry, spheremovieMaterial);
+const sphereA = new THREE.Mesh(sphereGeometry, spheremovieMaterial5);
 sphereA.position.set(-20, -8, 20);
 scene.add(sphereA);
 
-const sphereE = new THREE.Mesh(sphereGeometry, sphereMaterial);
+const sphereE = new THREE.Mesh(sphereGeometry, spheremovieMaterial4);
 sphereE.position.set(20, -8, 20);
 scene.add(sphereE);
 
-const sphereB = new THREE.Mesh(sphereGeometry, sphereMaterial);
+const sphereB = new THREE.Mesh(sphereGeometry, spheremovieMaterial1);
 sphereB.position.set(-10, -8, 20);
 scene.add(sphereB);
 
-const sphereC = new THREE.Mesh(sphereGeometry, sphereMaterial);
+const sphereC = new THREE.Mesh(sphereGeometry, spheremovieMaterial2);
 sphereC.position.set(0, -8, 20);
 scene.add(sphereC);
 
-const sphereD = new THREE.Mesh(sphereGeometry, sphereMaterial);
+const sphereD = new THREE.Mesh(sphereGeometry, spheremovieMaterial3);
 sphereD.position.set(10, -8, 20);
 scene.add(sphereD);
 
