d2b14f4822f83f4e7ac2d37a32ac8ac5f74d976e
Takanobu <61861946+takanobu68@users.noreply.github.com>
2021-03-17 03:22:13 +0900

    画面中央のSphereを追加、planeの位置調整と回転角度の調整

--- main.js
+++ main.js
@@ -11,13 +11,13 @@ const camera = new THREE.PerspectiveCamera(
   1,
   1000
 );
-camera.position.set(0, 0, 30);
+camera.position.set(0, 0, 50);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(container.clientWidth, container.clientHeight);
 container.appendChild(renderer.domElement);
 
-const geometry = new THREE.PlaneGeometry(20, 15);
+const geometry = new THREE.PlaneGeometry(30, 25);
 const material = new THREE.MeshBasicMaterial({
   color: 0xffff00,
   side: THREE.DoubleSide,
@@ -25,13 +25,20 @@ const material = new THREE.MeshBasicMaterial({
 const plane = new THREE.Mesh(geometry, material);
 const plane2 = new THREE.Mesh(geometry, material);
 
-plane.rotation.y = 10;
-plane.position.set(-12, 2, 0);
+plane.rotation.y = Math.PI / 4;
+plane.position.set(-20, 2, 0);
 scene.add(plane);
 
-plane2.rotation.y = -10;
-plane2.position.set(12, 2, 0);
+plane2.rotation.y = -Math.PI / 4;
+plane2.position.set(20, 2, 0);
 scene.add(plane2);
+
+const sphereGeometry = new THREE.SphereGeometry(3, 32, 32);
+const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+
+sphere.position.set(0, 0, 20);
+scene.add(sphere);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
