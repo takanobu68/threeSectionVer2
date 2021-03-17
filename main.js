614f9057f1ea5698d7560502363231a622b3cb41
Takanobu <61861946+takanobu68@users.noreply.github.com>
2021-03-18 06:32:59 +0900

    リンクの修正

--- main.js
+++ main.js
@@ -24,10 +24,10 @@ const movie3 = document.getElementById("three-video3");
 const movie4 = document.getElementById("three-video4");
 const movie5 = document.getElementById("three-video5");
 
-movie1.src = "./assets/video06.mp4";
+movie1.src = "./assets/video01.mp4";
 movie2.src = "./assets/video02.mp4";
 movie3.src = "./assets/video03.mp4";
-movie4.src = "./assets/video07.mp4";
+movie4.src = "./assets/video04.mp4";
 movie5.src = "./assets/video05.mp4";
 
 const videoTexture1 = new THREE.VideoTexture(movie1);
