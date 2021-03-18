e0ff9d24536b86a2847d8a913d0a4a80dfaadd4d
Takanobu <61861946+takanobu68@users.noreply.github.com>
2021-03-18 18:23:23 +0900

    init関数で全体を囲う init関数内の関数を分離

--- main.js
+++ main.js
@@ -2,38 +2,88 @@ import * as THREE from "./node_modules/three/build/three.module.js";
 import { OrbitControls } from "./node_modules/three/examples/jsm/controls/OrbitControls.js";
 import { gsap } from "./node_modules/gsap/all.js";
 
-const container = document.getElementById("scene-container");
+let scene,
+  container,
+  camera,
+  renderer,
+  rayCast,
+  controls,
+  centerSphere,
+  geometry,
+  material;
 
-const scene = new THREE.Scene();
+let saveTargetOriginalPos = [];
+let items = [];
+const mouse = new THREE.Vector2();
+let initialSettingVideoTexture;
 
-const camera = new THREE.PerspectiveCamera(
-  45,
-  container.clientWidth / container.clientHeight,
-  1,
-  1000
-);
-camera.position.set(0, 0, 50);
+function init() {
+  container = document.getElementById("scene-container");
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(container.clientWidth, container.clientHeight);
-container.appendChild(renderer.domElement);
+  scene = new THREE.Scene();
 
-// 動画が流れるオブジェクトの初期配置設定
-const initialSettingMovie = document.getElementById("initial-setting-movie");
-// srcはjs側で設定しないと不具合が出る
-initialSettingMovie.src = "./assets/video01.mp4";
-const initialSettingVideoTexture = new THREE.VideoTexture(initialSettingMovie);
+  camera = new THREE.PerspectiveCamera(
+    45,
+    container.clientWidth / container.clientHeight,
+    1,
+    1000
+  );
+  camera.position.set(0, 0, 50);
 
-// geometry,materialは変更の可能性があるので、詳細な命名はしない
-const geometry = new THREE.PlaneGeometry(30, 25);
-const material = new THREE.MeshBasicMaterial({
-  map: initialSettingVideoTexture,
-  side: THREE.DoubleSide,
-});
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(container.clientWidth, container.clientHeight);
+  container.appendChild(renderer.domElement);
 
-// Plane作成を関数化
-createPlane("left");
-createPlane("right");
+  // 動画が流れるオブジェクトの初期配置設定
+  const initialSettingMovie = document.getElementById("initial-setting-movie");
+  // srcはjs側で設定しないと不具合が出る
+  initialSettingMovie.src = "./assets/video01.mp4";
+  initialSettingVideoTexture = new THREE.VideoTexture(initialSettingMovie);
+
+  // geometry,materialは変更の可能性があるので、詳細な命名はしない
+  geometry = new THREE.PlaneGeometry(30, 25);
+  material = new THREE.MeshBasicMaterial({
+    map: initialSettingVideoTexture,
+    side: THREE.DoubleSide,
+  });
+
+  // Plane作成を関数化
+  createPlane("left");
+  createPlane("right");
+
+  // 中央に配置するsphereの設定
+  // sphereGeometryは他のsphereにも使用
+  const sphereGeometry = new THREE.SphereGeometry(3, 32, 32);
+  const centerSpheremovieMaterial = new THREE.MeshBasicMaterial({
+    map: initialSettingVideoTexture,
+  });
+  centerSphere = new THREE.Mesh(sphereGeometry, centerSpheremovieMaterial);
+  centerSphere.position.set(0, 0, 10);
+  scene.add(centerSphere);
+
+  // 手前に配置する5つのsphereの設定
+  // forEachによる一括配置
+  const frontSpheres = document.querySelectorAll(".front");
+
+  const frontSpherePos = [-20, -10, 0, 10, 20];
+  frontSpheres.forEach((el, i) => {
+    el.src = `./assets/video0${i + 1}.mp4`;
+    const videoTexture = new THREE.VideoTexture(el);
+    const videoMaterial = new THREE.MeshBasicMaterial({
+      map: videoTexture,
+    });
+    const sphere = new THREE.Mesh(sphereGeometry, videoMaterial);
+    sphere.position.set(frontSpherePos[i], -8, 20);
+    sphere.name = `sphere-${i + 1}`;
+    scene.add(sphere);
+  });
+
+  rayCast = new THREE.Raycaster();
+  items = [];
+
+  controls = new OrbitControls(camera, renderer.domElement);
+  container.addEventListener("click", startMovingImage);
+}
 
 function createPlane(position) {
   const planePos = position === "left" ? true : false;
@@ -42,46 +92,6 @@ function createPlane(position) {
   planePos ? plane.position.set(-20, 2, 0) : plane.position.set(20, 2, 0);
   scene.add(plane);
 }
-
-// 中央に配置するsphereの設定
-// sphereGeometryは他のsphereにも使用
-const sphereGeometry = new THREE.SphereGeometry(3, 32, 32);
-const centerSpheremovieMaterial = new THREE.MeshBasicMaterial({
-  map: initialSettingVideoTexture,
-});
-const centerSphere = new THREE.Mesh(sphereGeometry, centerSpheremovieMaterial);
-centerSphere.position.set(0, 0, 10);
-scene.add(centerSphere);
-
-// 手前に配置する5つのsphereの設定
-// forEachによる一括配置
-const frontSpheres = document.querySelectorAll(".front");
-
-const frontSpherePos = [-20, -10, 0, 10, 20];
-frontSpheres.forEach((el, i) => {
-  el.src = `./assets/video0${i + 1}.mp4`;
-  const videoTexture = new THREE.VideoTexture(el);
-  const videoMaterial = new THREE.MeshBasicMaterial({
-    map: videoTexture,
-  });
-  const sphere = new THREE.Mesh(sphereGeometry, videoMaterial);
-  sphere.position.set(frontSpherePos[i], -8, 20);
-  sphere.name = `sphere-${i + 1}`;
-  scene.add(sphere);
-});
-
-const rayCast = new THREE.Raycaster();
-const mouse = new THREE.Vector2();
-let items = [];
-const arrow = new THREE.ArrowHelper(
-  rayCast.ray.direction,
-  camera.position,
-  10,
-  0xff0000
-);
-scene.add(arrow);
-
-container.addEventListener("click", startMovingImage);
 
 function startMovingImage(e) {
   if (items.length) {
@@ -97,14 +107,11 @@ function startMovingImage(e) {
 
 function useRaycast() {
   rayCast.setFromCamera(mouse, camera);
-  arrow.setDirection(rayCast.ray.direction);
   items = rayCast.intersectObjects(scene.children);
   if (items[0].object.name) {
     items.forEach((target) => moveSphere(target));
   }
 }
-
-let saveTargetOriginalPos = [];
 
 function moveSphere(target) {
   const targetPos = target.object.position;
@@ -158,14 +165,11 @@ function goBack(targetPos) {
   });
 }
 
-const controls = new OrbitControls(camera, renderer.domElement);
-
-renderer.render(scene, camera);
-
-function draw() {
+function mainLoop() {
   initialSettingVideoTexture.needsUpdate = true;
   renderer.render(scene, camera);
-  requestAnimationFrame(draw);
+  requestAnimationFrame(mainLoop);
 }
 
-draw();
+init();
+mainLoop();
